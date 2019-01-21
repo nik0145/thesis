a3c13ba3e87f54e3af58170a9d552c0918a087f9
kolya_tyurin <kolya_tyurin@mail.ru>
2019-01-21 14:17:15 +0300

    сделал макет для будущего курсовика

--- app_client/app.js
+++ app_client/app.js
@@ -1,24 +1,29 @@
 (function() {
-    angular.module('myApp', ['ngRoute']);
+	angular.module('myApp', ['ngRoute']);
 
-    function config($routeProvider) {
+	function config($routeProvider) {
 
-        $routeProvider
-            .when('/', {
-                    templateUrl: 'index/index.html',
-                    controller: 'indexController',
-                    controllerAs: 'vm'
-                }
-            ).when('/page1', {
-                templateUrl: 'page1/page1.html',
-                controller: 'page1Controller',
-                controllerAs: 'vm'
-            }
-        ).otherwise({redirectTo: '/'});
+		$routeProvider
+			.when('/', {
+				templateUrl: 'index/index.html',
+				controller: 'indexController',
+				controllerAs: 'vm'
+				}
+			).when('/page1', {
+				templateUrl: 'page1/page1.html',
+				controller: 'page1Controller',
+				controllerAs: 'vm'
+				}
+			).when('/bootstrap', {
+				templateUrl: 'bootstrap/index.html',
+				controller: 'BstrpController',
+				controllerAs: 'vm'
+				}
+			).otherwise({redirectTo: '/'});
 
-    }
+	}
 
-    angular.module('myApp')
-        .config(['$routeProvider', config]);
+	angular.module('myApp')
+		.config(['$routeProvider', config]);
 })();
 
---
+++ app_client/bootstrap/BstrpController.js
@@ -0,0 +1,9 @@
+(function() {
+	function BstrpController() {
+		var vm = this;
+		vm.kek='ekkekekek';
+	}
+
+	angular.module('myApp')
+		.controller('BstrpController', BstrpController);
+})();
