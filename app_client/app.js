95533fd417867cb0df73e4cd589e1ccd8f9af979
kolya_tyurin <kolya_tyurin@mail.ru>
2019-05-30 00:53:20 +0300

    поменял порт починил автозаполненеие, поменял цвет

--- app_client/app.js
+++ app_client/app.js
@@ -80,20 +80,20 @@
 	config(function($mdThemingProvider) {
 
 		$mdThemingProvider.definePalette('amazingPaletteName', {
-			'50': 'ffebee',
-			'100': 'ffcdd2',
-			'200': 'ef9a9a',
-			'300': 'e57373',
-			'400': 'ef5350',
-			'500': 'f44336',
-			'600': 'e53935',
-			'700': 'd32f2f',
-			'800': 'c62828',
-			'900': 'b71c1c',
-			'A100': 'ff8a80',
-			'A200': 'ff5252',
-			'A400': 'ff1744',
-			'A700': 'd50000',
+			'50': '3C6395',
+			'100': '3C6395',
+			'200': '3C6395',
+			'300': '3C6395',
+			'400': '3C6395',
+			'500': '3C6395',
+			'600': '3C6395',
+			'700': '3C6395',
+			'800': '3C6395',
+			'900': '3C6395',
+			'A100': '3C6395',
+			'A200': '3C6395',
+			'A400': '3C6395',
+			'A700': '3C6395',
     'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
                                         // on this palette should be dark or light
 
